674f81aaaa114cf7baf581374bcb7574a5bc5e24
levilu800b <levilu800b@gmail.com>
2025-06-10 12:46:56 +0100

    Removed failed app

--- user_library/apps.py
+++
@@ -1,9 +0,0 @@
-# user_library/apps.py
-# type: ignore
-
-from django.apps import AppConfig
-
-class UserLibraryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user_library'
-    verbose_name = 'User Library'
